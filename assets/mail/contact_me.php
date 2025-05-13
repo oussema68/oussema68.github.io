c5cb1a5bbe2229052f896cf5dd1d83279290bd88
oussema hammadi <ohammadi.oh@gmail.com>
2025-05-13 13:36:11 +0200

    10th commit task3

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -16,22 +16,11 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'ohammadi.oh@gmail.com';
-$email_subject = "Website Contact Form: $name";
-$email_body = "You have received a new message from your website contact form.\n\n"
-    . "Here are the details:\n\n"
-    . "Name: $name\n\n"
-    . "Email: $email_address\n\n"
-    . "Phone: $phone\n\n"
-    . "Message:\n$message";
-
-$headers = "From: noreply@yourdomain.com\r\n";
-$headers .= "Reply-To: $email_address\r\n";
-
-// Send email
-if (mail($to, $email_subject, $email_body, $headers)) {
-    echo json_encode(['status' => 'success']);
-} else {
-    echo json_encode(['status' => 'error', 'message' => 'mail() failed']);
-}
-return;
+$to = 'yourname@yourdomain.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@yebi.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";   
+mail($to,$email_subject,$email_body,$headers);
+return true;         
+?>
